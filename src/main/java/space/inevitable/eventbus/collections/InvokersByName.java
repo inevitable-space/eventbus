c3045e13dd28b2f9d7ac5c8d0cace102dd815537
inevitable-space <david@inevitable.space>
2016-03-26 18:53:10 -0400

    Adding InvokersByName to improve the level of SRP, formatted code, executed PMD inspections, fixed missing final statements, found V-DL (Violations of the Demeter Law) pointed them as TODO

---
+++ src/main/java/space/inevitable/eventbus/collections/InvokersByName.java
@@ -0,0 +1,38 @@
+package space.inevitable.eventbus.collections;
+
+import space.inevitable.eventbus.invoker.Invoker;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class InvokersByName {
+    private final Map<String, Invoker> invokersByNameMap;
+
+    public InvokersByName() {
+        invokersByNameMap = new ConcurrentHashMap<>();
+    }
+
+    public void put(final String invokerName, final Invoker invoker) {
+        invokersByNameMap.put(invokerName, invoker);
+    }
+
+    public Invoker get(final String invokerName) {
+        final Invoker invoker = invokersByNameMap.get(invokerName);
+        validateInvokerExistence(invoker, invokerName);
+        return invoker;
+    }
+
+    private void validateInvokerExistence(final Invoker invoker, final String invokerName) {
+        if (invoker != null) {
+            return;
+        }
+
+        String message = "Invoker under the name: [" + invokerName + "] is not registered.";
+        message += "\n FIX: use the method addInvoker of the event bus.";
+        message += "\nSee the JavaDoc of the invoker interface.";
+        message += "\n FIX: use the method addInvoker of the event bus.";
+        message += "\nSee the JavaDoc of the invoker interface.";
+
+        throw new IllegalStateException(message);
+    }
+}

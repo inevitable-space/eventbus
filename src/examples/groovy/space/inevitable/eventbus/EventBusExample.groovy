9bab5e364796c1adf9f61f3b138ed824a91736cd
inevitable-space <david@inevitable.space>
2016-03-31 06:39:19 -0400

    Preparing for concurrency test, improved the way new sourceSets handle dependencies

--- src/examples/groovy/space/inevitable/eventbus/EventBusExample.groovy
+++ src/examples/groovy/space/inevitable/eventbus/EventBusExample.groovy
@@ -5,17 +5,16 @@ import spock.lang.Specification
 
 class EventBusExample extends Specification {
     def "How to set up and use"() {
-        given: //The minimum put up to use the event bus
+        given: 'The minimum set up to use the event bus'
 
         Builder<EventBus> standardEventBusBuilder = new StandardEventBusBuilder(); //notice that StandardEventBus needs to be created by the its builder
         EventBus eventBus = standardEventBusBuilder.build(); //notice that EventBus is an Interface that
 
-        when: //A instance of SubscriberClass (could be any of your classes) wants to register in to the bus
-
+        when: 'An instance of SubscriberClass (could be any of your classes) wants to register in to the bus'
         SubscriberClass subscriberClass = new SubscriberClass();
         eventBus.register(subscriberClass);
 
-        then: //the subscriberMethod will be invoked when a string is posted in to the bus
+        then: 'the subscriberMethod will be invoked when a string is posted in to the bus'
         eventBus.post("EVENT INSTANCE");
     }
 
